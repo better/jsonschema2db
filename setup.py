e9f91957d2af2cacd108efdd4755e496fce4f449
Erik Bernhardsson <erikbern@better.com>
2017-08-16 11:32:06 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='JSONSchema2DB',
+      version='1.0',
+      description='Generate database tables from JSON schema',
+      author='Erik Bernhardsson',
+      author_email='mail@erikbern.com',
+      packages=['jsonschema2db'],
+      install_requires=[
+          'change_case==0.5.2',
+          'iso8601==0.1.12',
+          'psycopg2==2.7.2'
+      ])
